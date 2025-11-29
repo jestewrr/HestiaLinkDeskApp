db3ef4a7114b38a5b89bf80208ee1611309480dc
GEOTHANo <g.olano.544130@umindanano.edu.ph>
2025-11-30 03:08:09 +0800

    add department crud

---
+++ Data/HestiaLinkContext.cs
@@ -0,0 +1,22 @@
+﻿using HestiaLink.Models;  // Fixed namespace
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace HestiaLink.Data
+{
+    public class HestiaLinkContext : DbContext
+    {
+        public HestiaLinkContext(DbContextOptions<HestiaLinkContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Department> Departments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // This tells Entity Framework to use your existing SQL Server table
+            modelBuilder.Entity<Department>().ToTable("Department");
+        }
+    }
+}
