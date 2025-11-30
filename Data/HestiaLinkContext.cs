120f514eed4686fcf473e4af20fb0a8104297082
Geoff <Geoff@MSI>
2025-12-01 00:21:01 +0800

    added function for HR

--- Data/HestiaLinkContext.cs
+++ Data/HestiaLinkContext.cs
@@ -12,11 +12,17 @@ namespace HestiaLink.Data
         }
 
         public DbSet<Department> Departments { get; set; }
+        public DbSet<Position> Positions { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<SystemUser> SystemUsers { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             // This tells Entity Framework to use your existing SQL Server table
             modelBuilder.Entity<Department>().ToTable("Department");
+            modelBuilder.Entity<Position>().ToTable("Position");
+            modelBuilder.Entity<Employee>().ToTable("Employee");
+            modelBuilder.Entity<SystemUser>().ToTable("SystemUser");
         }
     }
 }
